c240029451aec6249df54746312675c81bf562d9
Chaimaa El Ghandori <119333031+Maeshai@users.noreply.github.com>
2023-08-29 11:21:40 +0100

    Update ft_putnbr.c

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -18,13 +18,13 @@ void	ft_putchar(char c)
 void	ft_putnbr(int nb)
 {	
 	if (nb == -2147483648)
-	{
-		write (1, "-2147483648", 11);
-	}
+	ft_putnbr(-2147483648);
+	
 	if (nb < 0)
 	{
-		ft_putchar("-");
+		ft_putchar('-');
 		nb = -nb;
+		ft_putnbr(nb);
 	}
 	if (nb < 10)
 	{
@@ -33,6 +33,6 @@ void	ft_putnbr(int nb)
 	else
 	{
 		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10 + '0');
+		ft_putnbr(nb % 10);
 	}
 }
